5ea25b5e561713d2a7dd21948a408472e3eec28c
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2019-05-22 16:31:22 +0200

    [CI] Attempt to launch the Airflow pods in Travis

--- ci-scripts/integration_tests.sh
+++ ci-scripts/integration_tests.sh
@@ -7,5 +7,8 @@ kubectl --kubeconfig="$(kind get kubeconfig-path)" get nodes
 echo "K8S pods"
 kubectl --kubeconfig="$(kind get kubeconfig-path)" get pods --all-namespaces -o wide
 
+echo "Launching Airflow pods"
+kubectl --kubeconfig="$(kind get kubeconfig-path)" apply -f k8s
+
 # kubectl get pods --all-namespaces -o wide
 
